90a9daff7cdabb4291a526d8fd1c6b12fca4b726
yuhuashi111 <42089575+yuhuashi111@users.noreply.github.com>
2020-07-31 21:02:35 +0800

    Update 1114. Family Property (25).cpp

--- AdvancedLevel_C++/1114. Family Property (25).cpp
+++ AdvancedLevel_C++/1114. Family Property (25).cpp
@@ -2,14 +2,14 @@
 #include <algorithm>
 using namespace std;
 struct DATA {
-    int id, fid, mid, num, area;//中年人作根
+    int id, fid, mid, num, area;
     int cid[10];//儿子
-}data[1005];
+}data[1005];//输入形式
 struct node {
     int id, people;
     double num, area;
     bool flag = false;//统计家庭数，检验某家庭是否存在
-}ans[10000];
+}ans[10000];//输出形式，一个ans[]元素是一个合并后的家
 int father[10000];
 bool visit[10000];//统计家庭人数
 int find(int x) {
@@ -34,9 +34,9 @@ int cmp1(node a, node b) {
 int main() {
     int n, k, cnt = 0;
     scanf("%d", &n);
-    for(int i = 0; i < 10000; i++)
+    for(int i = 0; i < 10000; i++)//初
         father[i] = i;
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {//I
         scanf("%d %d %d %d", &data[i].id, &data[i].fid, &data[i].mid, &k);
         visit[data[i].id] = true;
         if(data[i].fid != -1) {
@@ -54,27 +54,27 @@ int main() {
         }
         scanf("%d %d", &data[i].num, &data[i].area);
     }
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {//转
         int id = find(data[i].id);
-        ans[id].id = id;
+        ans[id].id = id;//输出的下标及家的根为合并后的根
         ans[id].num += data[i].num;
         ans[id].area += data[i].area;
         ans[id].flag = true;
     }
-    for(int i = 0; i < 10000; i++) {
+    for(int i = 0; i < 10000; i++) {//数
         if(visit[i])
             ans[find(i)].people++;
         if(ans[i].flag)
             cnt++;
     }
-    for(int i = 0; i < 10000; i++) {
+    for(int i = 0; i < 10000; i++) {//算
         if(ans[i].flag) {
             ans[i].num = (double)(ans[i].num * 1.0 / ans[i].people);
             ans[i].area = (double)(ans[i].area * 1.0 / ans[i].people);
         }
     }
-    sort(ans, ans + 10000, cmp1);
-    printf("%d\n", cnt);
+    sort(ans, ans + 10000, cmp1);//排
+    printf("%d\n", cnt);//O
     for(int i = 0; i < cnt; i++)
         printf("%04d %d %.3f %.3f\n", ans[i].id, ans[i].people, ans[i].num, ans[i].area);
     return 0;
